4dd34143a36565fa6adad8394a8f3db6ba86bb7e
prodeveloper <dany.ourapps99@gmail.com>
2020-04-10 18:02:30 +1000

    works fine

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react'
 import { SafeAreaView, Text, StyleSheet, View, ImageBackground, TouchableOpacity } from 'react-native'
 import { Button } from 'react-native-paper'
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
+import registerRootComponent from 'expo/build/launch/registerRootComponent'
 import Home from './Screens/Home'
 import Feed from './Screens/Feed'
-const Drawer = createDrawerNavigator()
+const Drawer = createStackNavigator()
 export default class App extends React.Component {
   render() {
     return (
@@ -17,13 +20,11 @@ class Navigation extends React.Component {
   render() {
     return (
       <NavigationContainer>
-        <Drawer.Navigator statusBarAnimation="fade" drawerPosition="left" drawerType="slide" drawerStyle={{
-          backgroundColor: '#2FABBC',
-          width: 260,
-
-        }} >
-          <Drawer.Screen name="Home" component={Home} options={{}} />
-          <Drawer.Screen name="Feed" component={Feed} options={{}} />
+        <Drawer.Navigator>
+          <Drawer.Screen name="Home" component={Home} options={{
+            headerShown: false,
+          }} />
+          <Drawer.Screen name="Feed" component={Feed} options={{ headerShown: false }} />
         </Drawer.Navigator>
       </NavigationContainer>
     );
---
+++ assets/Files/Api.js
@@ -0,0 +1,81 @@
+const workouts = [
+    {
+        name: 'Rowing',
+        id: 0,
+        sets: '2 sets of 10',
+        time: 10,
+        // img: '../assets/1.png'
+
+    },
+    {
+        name: 'Push-ups',
+        id: 1,
+        sets: '2 sets of 10',
+        time: 8,
+        // img: '../assets/1.png'
+
+    },
+    {
+        name: 'Sit-ups',
+        id: 2,
+        sets: '5 sets of 8',
+        time: 4,
+        // img: '../assets/1.png'
+
+    },
+    {
+        name: 'Toe-touch',
+        id: 3,
+        sets: '1 set of hundred',
+        time: 6,
+        // img: '../assets/1.png'
+
+    },
+    {
+        name: 'Side-stretch',
+        id: 4,
+        sets: '10 sets of 10',
+        time: 3,
+        // img: '../assets/1.png'
+    },
+    {
+        name: 'Dumbells-Pushup',
+        id: 5,
+        sets: '1 set of 5',
+        time: 1,
+        // img: '../assets/1.png'
+    },
+    {
+        name: 'Dumbbell',
+        id: 6,
+        sets: '1 set of 5',
+        time: 7,
+        // img: '../assets/1.png'
+    },
+    {
+        name: 'Stand-still',
+        id: 7,
+        sets: '1 set of 5',
+        time: 1,
+        // img: '../assets/1.png'
+    },
+    {
+        name: 'Weight-lift',
+        id: 8,
+        sets: '1 set of 5',
+        time: 2.5,
+        // img: '../assets/1.png'
+    },
+    {
+        name: 'Leg-Swissball',
+        id: 9,
+        sets: '1 set of 5',
+        time: 2.5,
+        // img: '../assets/1.png'
+    },
+
+
+
+]
+
+module.exports.workouts = workouts;
